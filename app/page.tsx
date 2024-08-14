7163518f1d5f07ab8ffba0eab693eeb0d4860441
AgawaToshiki <t.agawa1128ht@gmail.com>
2024-08-15 00:05:20 +0900

    コンポーネント化の前のコード削除

--- app/page.tsx
+++ app/page.tsx
@@ -15,14 +15,6 @@ export default async function DashBoard() {
           <SignOutButton/>
         </div>
         <div className="flex bg-blue-100">
-          {data?.map((user) => (
-            <div className="flex items-center mx-4 p-4 border border-black rounded-lg bg-red-100" key={user.id}>
-              <div className="p-4 bg-red-100">{user.displayName}</div>
-              <div className={`w-4 h-4 rounded-full ${user.status === 'online' ? 'bg-green-400' : user.status === 'leave' ? 'bg-red-400' : 'bg-gray-400'}`}></div>
-            </div>
-          ))}
-        </div>
-        <div className="flex bg-blue-100">
           <UserList data={data}/>
         </div>
       </div>
