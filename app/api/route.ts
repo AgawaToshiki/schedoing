3f3bc89558d6fa6acaf84ae789e4f2e0e0c5bce3
Agawa <t.agawa1128ht@gmail.com>
2024-08-16 13:54:16 +0900

    テストapi削除

--- app/api/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  return NextResponse.json([
-    { id: 1, message: "データ1" },
-    { id: 2, message: "データ2" },
-    { id: 3, message: "データ3" }
-  ]);
-}
