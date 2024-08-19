45d958bb6abf33fb338fa5d6922f0235a4b25708
AgawaToshiki <t.agawa1128ht@gmail.com>
2024-08-19 21:40:29 +0900

    一部の処理をAPIからServerActionsへ変更

--- app/api/user/update/route.ts
+++
@@ -1,9 +0,0 @@
-import { type NextRequest, NextResponse } from 'next/server'
- 
-export default function POST(req: NextRequest, res: NextResponse) {
-  try {
-    
-  }catch {
-
-  }
-}
--- app/components/DeleteButton.tsx
+++ app/components/DeleteButton.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
-
+import { deleteUserFromAuth } from '../utils/authAdmin';
+import { deleteUser } from '../utils/supabaseFunctions';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 
 type Props = {
   id: string;
@@ -7,9 +10,22 @@ type Props = {
 
 const DeleteButton = ({ id }: Props) => {
 
+	const handleDelete = async() => {
+		'use server'
+		try {
+			if(!id) throw new Error();
+			await deleteUserFromAuth(id);
+			await deleteUser(id);
+		}catch (error) {
+			console.error("DeleteUser Error:", error)
+		}
+		revalidatePath('/user')
+		redirect('/user')
+	}
+
   return (
 		<>
-			<form action={`../api/auth/delete/${id}`} method="post">
+			<form action={handleDelete}>
 				<button className="p-1 border bg-red-400">削除</button>
 			</form>
 		</>
