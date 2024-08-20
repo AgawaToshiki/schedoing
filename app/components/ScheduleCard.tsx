b3d0f4d9116e151422dc6a4fcf381c4cdb1971ed
AgawaToshiki <t.agawa1128ht@gmail.com>
2024-08-21 02:12:09 +0900

    カードの表示位置の設定

--- app/components/ScheduleCard.tsx
+++ app/components/ScheduleCard.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { format } from "date-fns";
 
 type Props = {
   schedule: {
@@ -9,13 +10,29 @@ type Props = {
   }
 }
 
-const ScheduleCard = ({schedule}: Props) => {
+const ScheduleCard = ({ schedule }: Props) => {
+  const startTime = new Date(schedule.start_time)
+  const s = startTime.getHours();
+  const ss = startTime.getMinutes();
+  const sss = s * 60 + ss;
+
+  const endTime = new Date(schedule.end_time)
+  const e = endTime.getHours();
+  const ee = endTime.getMinutes();
+  const eee = e * 60 + ee;
+
+  const height = eee - sss
+  console.log(height);
+  console.log(sss);
+  // const formatDate = format(schedule.start_time, "yyyy/MM/dd");
   return (
     <>
-      <div className="flex flex-col bg-green-400">
+      <div 
+        className="flex items-center justify-center gap-10 absolute w-full bg-green-400"
+        style={{height:`${height}px`, top:`${sss}px`}}
+      >
         <div>{schedule.title}</div>
-        <div>{schedule.start_time.toString()}</div>
-        <div>{schedule.end_time.toString()}</div>
+        <button>Delete</button>
       </div>
     </>
   )
