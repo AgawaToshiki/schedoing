577cc13f921249f003a26fc4e8197fe4e181b910
Agawa <t.agawa1128ht@gmail.com>
2024-08-22 15:51:17 +0900

    現在時刻のボーダーへ遷移

--- app/components/UserList.tsx
+++ app/components/UserList.tsx
@@ -41,7 +41,7 @@ const UserList = ({ data }: Props) => {
   return (
     <>
       {users?.map((user) => (
-        <Link href={`/${user.id}/schedule`} key={user.id}>        
+        <Link href={`/${user.id}/schedule#currentTimeBorder`} key={user.id}>        
           <div className="flex items-center gap-4 mx-4 p-6 border border-gray-200 rounded-md shadow-md bg-white">
             <div>{user.displayName}</div>
             <div className={`w-4 h-4 rounded-full ${user.status === 'online' ? 'bg-green-400' : user.status === 'leave' ? 'bg-red-400' : 'bg-gray-400'}`}></div>
