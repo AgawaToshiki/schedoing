359d609aa7ec2c360f9b5ca6749df5f777d2362b
AgawaToshiki <t.agawa1128ht@gmail.com>
2024-09-07 20:38:17 +0900

    テーブルレイアウト修正

--- app/components/AdminUserList.tsx
+++ app/components/AdminUserList.tsx
@@ -8,7 +8,7 @@ const AdminUserList = async() => {
 
   return (
 		<>
-			<div className="max-w-[800px] flex bg-white overflow-y-auto">
+			{/* <div className="max-w-[800px] flex bg-white overflow-y-auto">
 				<div className="flex-grow overflow-x-auto">
 					<table className="w-full border-collapse">
 						<thead>
@@ -50,7 +50,58 @@ const AdminUserList = async() => {
 						</tbody>
 					</table>
 				</div>
-			</div>
+			</div> */}
+			        <div className="max-w-[800px] h-full flex bg-white">
+          <div className="flex-grow overflow-x-auto h-full overflow-y-scroll">
+            <table className="w-full h-[200%] border border-collapse">
+              <thead>
+                <tr className="">
+                  <th className="
+                  sticky top-0 z-20 min-w-[150px] px-4 py-2 bg-gray-100 border text-left whitespace-nowrap
+                  before:absolute before:top-[-1px] before:left-[-1px] before:w-full before:h-full before:border-t
+                  ">
+                    権限
+                  </th>
+                  <th className="
+                  sticky top-0 z-20 min-w-[250px] px-4 py-2 bg-gray-100 border text-left whitespace-nowrap
+                  before:absolute before:top-[-1px] before:left-[-1px] before:w-full before:h-full before:border-t
+                  ">
+                    ユーザー名
+                  </th>
+                  <th className="
+                  sticky top-0 z-20 min-w-[250px] px-4 py-2 bg-gray-100 border text-left whitespace-nowrap
+                  before:absolute before:top-[-1px] before:left-[-1px] before:w-full before:h-full before:border-t
+                  ">
+                    メールアドレス
+                  </th>
+                  <th className="
+                  sticky top-0 right-0 z-20 min-w-[150px] px-4 py-2 bg-gray-100 border text-left whitespace-nowrap
+                  before:absolute before:top-[-1px] before:right-[-1px] before:w-full before:h-full before:border-t before:border-r
+                  ">
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {data?.map((user) => (
+                  <tr key={user.id} className="">
+                    <td className="px-4 py-2 border whitespace-nowrap">{user.role}</td>
+                    <td className="px-4 py-2 border whitespace-nowrap">{user.displayName}</td>
+                    <td className="px-4 py-2 border whitespace-nowrap">{user.email}</td>
+                    <td className="
+                    sticky right-0 z-10 px-4 py-2 border bg-white whitespace-nowrap
+                    before:absolute before:z-10 before:top-[-1px] before:right-[-1px] before:w-full before:h-full before:border-r
+                    ">
+                      <div className="relative z-50 flex justify-center items-center gap-2">
+                        <EditUserButton user={user} />
+                        <DeleteUserButton id={user.id} />
+                      </div>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
 		</>
   )
 }
