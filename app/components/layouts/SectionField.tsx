81edd88c9bce1c5d113d93d21638253b03af3a4b
AgawaToshiki <t.agawa1128ht@gmail.com>
2024-09-13 00:19:22 +0900

    不要なスタイルの削除

--- app/components/layouts/SectionField.tsx
+++ app/components/layouts/SectionField.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const SectionField = ({ sectionTitle, children }: Props) => {
   return (
-    <div className="flex flex-col justify-center w-full h-full p-6 border border-gray-200 rounded-md shadow-md bg-white">
+    <div className="flex flex-col w-full h-full p-6 border border-gray-200 rounded-md shadow-md bg-white">
       <div className="mb-6">
         <h2>{sectionTitle}</h2>
       </div>
