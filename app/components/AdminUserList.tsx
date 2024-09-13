fcbad131247221aa03548cc3ed205ce84a7412d2
Agawa <t.agawa1128ht@gmail.com>
2024-09-13 12:30:55 +0900

    テーブルのボーダーの修正

--- app/components/AdminUserList.tsx
+++ app/components/AdminUserList.tsx
@@ -8,45 +8,50 @@ const AdminUserList = async() => {
 
   return (
 		<>
-      <div className="relative max-w-[800px] h-full overflow-x-auto flex bg-white">
-        <div className="absolute flex-grow h-full">
-          <table className="w-full h-full overflow-y-auto border border-collapse">
-            <thead className="h-full">
-              <tr className="h-[50px]">
+      <div className="w-full h-full overflow-x-auto flex bg-white">
+        <div className="relative flex-grow h-full overflow-y-auto">
+          <table className="absolute w-full h-full border border-collapse">
+            <thead>
+              <tr>
                 <th className="
-                sticky top-0 z-20 min-w-[150px] px-4 py-2 bg-gray-100 border text-left whitespace-nowrap
+                sticky top-0 z-20 min-w-[150px] px-4 py-2 bg-gray-100 border text-center whitespace-nowrap
                 before:absolute before:top-[-1px] before:left-[-1px] before:w-full before:h-full before:border-t
+                after:absolute after:z-10 after:left-0 after:bottom-[-1px] after:border-b after:w-full after:h-full
                 ">
                   権限
                 </th>
                 <th className="
-                sticky top-0 z-20 min-w-[250px] px-4 py-2 bg-gray-100 border text-left whitespace-nowrap
+                sticky top-0 z-20 min-w-[250px] px-4 py-2 bg-gray-100 border text-center whitespace-nowrap
                 before:absolute before:top-[-1px] before:left-[-1px] before:w-full before:h-full before:border-t
+                after:absolute after:z-1 after:left-0 after:bottom-[-1px] after:border-b after:w-full after:h-full
                 ">
                   ユーザー名
                 </th>
                 <th className="
-                sticky top-0 z-20 min-w-[250px] px-4 py-2 bg-gray-100 border text-left whitespace-nowrap
+                sticky top-0 z-20 min-w-[250px] px-4 py-2 bg-gray-100 border text-center whitespace-nowrap
                 before:absolute before:top-[-1px] before:left-[-1px] before:w-full before:h-full before:border-t
+                after:absolute after:z-10 after:left-0 after:bottom-[-1px] after:border-b after:w-full after:h-full
                 ">
                   メールアドレス
                 </th>
                 <th className="
-                sticky top-0 right-0 z-20 min-w-[150px] px-4 py-2 bg-gray-100 border text-left whitespace-nowrap
+                sticky top-0 right-0 z-20 min-w-[150px] px-4 py-2 bg-gray-100 border text-center whitespace-nowrap
                 before:absolute before:top-[-1px] before:right-[-1px] before:w-full before:h-full before:border-t before:border-r
+                after:absolute after:z-10 after:left-[-1px] after:bottom-[-1px] after:border-l after:border-b after:w-full after:h-full
                 ">
                 </th>
               </tr>
             </thead>
-            <tbody className="h-full">
+            <tbody>
               {data?.map((user) => (
-                <tr key={user.id} className="h-[50px]">
+                <tr key={user.id}>
                   <td className="px-4 py-2 border whitespace-nowrap">{user.role}</td>
                   <td className="px-4 py-2 border whitespace-nowrap">{user.displayName}</td>
                   <td className="px-4 py-2 border whitespace-nowrap">{user.email}</td>
                   <td className="
                   sticky right-0 z-10 px-4 py-2 border bg-white whitespace-nowrap
                   before:absolute before:z-10 before:top-[-1px] before:right-[-1px] before:w-full before:h-full before:border-r
+                  after:absolute after:z-10 after:top-[-1px] after:left-[-1px] after:border-l after:w-full after:h-full
                   ">
                     <div className="relative z-50 flex justify-center items-center gap-2">
                       <EditUserButton user={user} />
