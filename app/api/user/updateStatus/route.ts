081316e9f144ba3d5334102b7684dd08267a1120
Agawa <t.agawa1128ht@gmail.com>
2024-10-24 10:15:41 +0900

    apiのバリデーション追加

--- app/api/user/updateStatus/route.ts
+++ app/api/user/updateStatus/route.ts
@@ -1,9 +1,20 @@
 'use server'
 import { NextRequest, NextResponse } from "next/server";
-import { updateStatus } from "@/app/utils/supabase/supabaseFunctions";
+import { getUser, updateStatus } from "@/app/utils/supabase/supabaseFunctions";
+import { getCurrentUser } from "@/app/utils/supabase/auth";
 
 export async function POST(req: NextRequest, res: NextResponse) {
   try {
+
+    const authUser = await getCurrentUser();
+    if(!authUser || !authUser.id){
+      return NextResponse.json({ error: 'Unauthorized User' }, { status: 401 });
+    }
+    const user = await getUser(authUser.id);
+    if(!user){
+      return NextResponse.json({ error: 'Unauthorized User' }, { status: 401 });
+    }
+    
     const data: { id: string, status: string } = await req.json();
 
     if(!data.id) throw new Error("User not found");
