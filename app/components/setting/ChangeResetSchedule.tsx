b5fd4f6b910cb6ff9573d35911a3813117bf8557
Agawa <t.agawa1128ht@gmail.com>
2024-11-11 13:00:57 +0900

    トグルの変更タイミングをずらした

--- app/components/setting/ChangeResetSchedule.tsx
+++ app/components/setting/ChangeResetSchedule.tsx
@@ -1,6 +1,6 @@
 'use client'
-import React, { useState } from 'react'
-import { Field, Label, Switch } from '@headlessui/react'
+import React, { useState } from 'react';
+import { Field, Label, Switch } from '@headlessui/react';
 
 
 type Props = {
@@ -11,11 +11,14 @@ type Props = {
 const base_url = process.env.NEXT_PUBLIC_BASE_URL;
 
 const ChangeResetSchedule = ({ id, resetFlag }: Props) => {
+
   const [enabled, setEnabled] = useState<boolean>(resetFlag);
 
   const handleChangeToggle = async() => {
 
     const newResetFlag = !enabled;
+
+    setEnabled(newResetFlag);
 
     try {
       const response = await fetch(`${base_url}/api/users/${id}/reset-schedules`, {
@@ -32,36 +35,51 @@ const ChangeResetSchedule = ({ id, resetFlag }: Props) => {
       if(!response.ok) {
 				console.error(response.status, data.error);
 				alert(`${response.status}:${data.error}`);
+        setEnabled(!newResetFlag);
         return
 			}
 
-      setEnabled(newResetFlag);
+
     } catch(error) {
       console.error("fetch Error:", error);
       alert("変更に失敗しました。ネットワーク接続を確認してください。");
+      setEnabled(!newResetFlag);
     }
   }
 
   return (
     <>
-      <form action="">
-        <Field>
-          <Label>
-            日付変更時にスケジュールをリセット
-          </Label>
-          <Switch
-            checked={enabled}
-            onChange={handleChangeToggle}
-            name="toggle"
-            className="group inline-flex items-center h-6 w-12 cursor-pointer rounded-full bg-gray-200 data-[checked]:bg-green-500 p-1 transition-colors duration-200 ease-in-out"
-          >
-            <span
-              aria-hidden="true"
-              className="pointer-events-none inline-block size-5 translate-x-0 rounded-full bg-white ring-0 shadow-lg transition duration-200 ease-in-out group-data-[checked]:translate-x-5"
-            />
-          </Switch>
-        </Field>
-      </form>
+      <Field className="flex items-center gap-1.5">
+        <Label>
+          日付変更時にスケジュールをリセット
+        </Label>
+        <Switch
+          checked={enabled}
+          onChange={handleChangeToggle}
+          name="toggle"
+          className="group inline-flex items-center h-6 w-12 cursor-pointer rounded-full bg-gray-200 data-[checked]:bg-green-500 p-1 transition-colors duration-200 ease-in-out"
+        >
+          <span
+            aria-hidden="true"
+            className="pointer-events-none inline-block size-5 translate-x-0 rounded-full bg-white ring-0 shadow-lg transition duration-200 ease-in-out group-data-[checked]:translate-x-5"
+          />
+        </Switch>
+      </Field>
+      {/* <div className="flex items-center gap-1.5">
+        <div>日付変更時にスケジュールをリセット</div>
+        <Switch
+          checked={enabled}
+          onChange={handleChangeToggle}
+          name="toggle"
+          className="group inline-flex items-center h-6 w-12 cursor-pointer rounded-full bg-gray-200 data-[checked]:bg-green-500 p-1 transition-colors duration-200 ease-in-out"
+        >
+          <span
+            aria-hidden="true"
+            className="pointer-events-none inline-block size-5 translate-x-0 rounded-full bg-white ring-0 shadow-lg transition duration-200 ease-in-out group-data-[checked]:translate-x-5"
+          />
+        </Switch>
+      </div> */}
+
     </>
   )
 }
