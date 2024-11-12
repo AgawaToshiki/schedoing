45d3344b5667509db2802533ae88241b7f58fc73
AgawaToshiki <t.agawa1128ht@gmail.com>
2024-11-13 02:22:49 +0900

    エラーページのレイアウト制作

---
+++ app/components/BackToHomeLink.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Link from 'next/link'
+
+const BackToHomeLink = () => {
+  return (
+    <>
+      <Link 
+        href="/"
+        className="inline-flex justify-center items-center px-3 py-1.5 text-sm/6 text-black font-semibold border border-gray-500 rounded-md bg-white"
+      >
+        ホームへ戻る
+      </Link>
+    </>
+  )
+}
+
+export default BackToHomeLink
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,37 @@
 import Link from 'next/link'
+import { getCurrentUser } from './utils/supabase/auth';
+import { redirect } from 'next/navigation';
+import { getUser } from './utils/supabase/supabaseFunctions';
+import { isAdminUser } from './utils/validation';
+import Main from './components/layouts/Main';
+import SectionField from './components/layouts/SectionField';
  
-export default function NotFound() {
+export default async function NotFound() {
+  const authUser = await getCurrentUser();
+  if(!authUser || !authUser.id){
+		redirect('/login')
+	}
+  const user = await getUser(authUser.id);
+  if(!user){
+    redirect('/login')
+  }
+  const isAdmin = isAdminUser(user);
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
-    </div>
+    <>
+      <Main isAdmin={isAdmin} id={authUser.id}>
+        <SectionField sectionTitle="404 Not Found">
+          <div>
+            <p className="mb-2">ページが見つかりませんでした。</p>
+            <Link 
+              href="/"
+              className="inline-flex justify-center items-center px-3 py-1.5 text-sm/6 text-black font-semibold border border-gray-500 rounded-md bg-white"
+            >
+              ホームへ戻る
+            </Link>
+          </div>
+        </SectionField>
+      </Main>
+    </>
+
   )
 }
