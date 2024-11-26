847ce5dec55ea75ec74f8c7d889309039d4de0e6
Agawa <t.agawa1128ht@gmail.com>
2024-11-26 09:20:17 +0900

    UserList表示にSuspenseの追加

--- app/(main)/user/page.tsx
+++ app/(main)/user/page.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { redirect } from 'next/navigation';
 import { getAllUser, getUser } from '../../utils/supabase/supabaseFunctions';
 import RegisterUser from '../../components/user/RegisterUser'
@@ -6,6 +6,23 @@ import AdminUserList from '../../components/user/AdminUserList'
 import SectionField from '../../components/layouts/SectionField';
 import { getCurrentUser } from '@/app/utils/supabase/auth';
 import { isAdminUser } from '@/app/utils/validation';
+import Loading from '@/app/loading';
+
+
+const UserList = async() => {
+	const data = await getAllUser();
+	if(!data) {
+		throw new Error("User does not exist");
+	}
+
+	return (
+		<>
+			<SectionField sectionTitle="ユーザー一覧">
+				<AdminUserList data={data} />
+			</SectionField>
+		</>
+	)
+}
 
 
 const User = async() => {
@@ -35,9 +52,12 @@ const User = async() => {
 					<RegisterUser />
 				</SectionField>
 			</div>
-			<SectionField sectionTitle="ユーザー一覧">
+			{/* <SectionField sectionTitle="ユーザー一覧">
 				<AdminUserList data={data} />
-			</SectionField>
+			</SectionField> */}
+			<Suspense fallback={<Loading />}>
+				<UserList />
+			</Suspense>
 		</>
 	)
 }
