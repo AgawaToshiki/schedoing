953b4a56adb63d663c4248f276322fcf51154063
Agawa <t.agawa1128ht@gmail.com>
2024-11-26 12:37:42 +0900

    ローディングUI表示の分離

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
+import Loading from './components/layouts/Loading';
 
-const Loading = () => {
+const LoadingPage = () => {
   return(
-    <div className="flex justify-center items-center gap-6 w-full min-h-screen p-6 bg-blue-100">
-      <div className="w-6 h-6 border-t-2 border-l-2 rounded-full border-blue-400 animate-rotate-center"></div>
-      <p className="text-xl">Loading</p>
+    <div className="flex justify-center items-center w-full min-h-screen p-6 bg-blue-100">
+      <Loading />
     </div>
   )
 }
 
-export default Loading
+export default LoadingPage
