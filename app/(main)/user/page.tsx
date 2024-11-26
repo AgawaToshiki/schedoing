3ee0c7a093edfd98040fc941126335845174ce99
Agawa <t.agawa1128ht@gmail.com>
2024-11-26 09:04:15 +0900

    権限チェックの追加

--- app/(main)/user/page.tsx
+++ app/(main)/user/page.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
-import { getAllUser } from '../../utils/supabase/supabaseFunctions';
+import { redirect } from 'next/navigation';
+import { getAllUser, getUser } from '../../utils/supabase/supabaseFunctions';
 import RegisterUser from '../../components/user/RegisterUser'
 import AdminUserList from '../../components/user/AdminUserList'
 import SectionField from '../../components/layouts/SectionField';
+import { getCurrentUser } from '@/app/utils/supabase/auth';
+import { isAdminUser } from '@/app/utils/validation';
 
 
 const User = async() => {
+
+	const authUser = await getCurrentUser();
+  if(!authUser || !authUser.id){
+		redirect('/login')
+	}
+  const user = await getUser(authUser.id);
+  if(!user){
+    redirect('/login')
+  }
+  const isAdmin = isAdminUser(user);
+	if(!isAdmin) {
+		redirect('/')
+	}
 
 	const data = await getAllUser();
   if(!data) {
