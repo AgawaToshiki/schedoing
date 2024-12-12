3971e627a60b2a60cda882544a5313a5819ab773
Agawa <t.agawa1128ht@gmail.com>
2024-12-12 10:13:40 +0900

    iOSのmin-height 100vh対応

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,7 +6,7 @@ export default function NotFound() {
 
   return (
     <>
-      <div className="w-full min-h-screen p-6 bg-blue-100">
+      <div className="w-full min-h-screen ios-min-height p-6 bg-blue-100">
         <SectionField sectionTitle="404 Not Found">
           <div>
             <p className="mb-2">ページが見つかりませんでした。</p>
