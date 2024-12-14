a3513afaefe263353cc545e774f02860a9618832
AgawaToshiki <t.agawa1128ht@gmail.com>
2024-12-15 00:27:21 +0900

    レイアウト修正

--- app/components/layouts/Navigation.tsx
+++ app/components/layouts/Navigation.tsx
@@ -76,7 +76,7 @@ const Navigation = ({ isAdmin, id }: Props) => {
               <li key={list.href}>
                 <Link 
                   href={list.href}
-                  className={`${list.pathname === pathname && "bg-blue-500 font-semibold"} flex items-center justify-center rounded-full px-2 py-2 transition duration-200 ease-in-out cursor-pointer hover:bg-blue-500`}
+                  className={`${list.pathname === pathname && "bg-blue-500 font-semibold"} flex items-center justify-center rounded-full w-[40px] h-[40px] px-2 py-2 transition duration-200 ease-in-out cursor-pointer hover:bg-blue-500`}
                 >
                   <Icon icon={list.icon} color="#fff" size={20} />
                 </Link>
