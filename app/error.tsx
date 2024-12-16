d75b0dd1720d1ff18fd07167c62a2db299b5a0ef
AgawaToshiki <t.agawa1128ht@gmail.com>
2024-12-16 19:50:54 +0900

    エラーページ、ローディングページのスマホサイズのレイアウト調整

--- app/error.tsx
+++ app/error.tsx
@@ -14,7 +14,7 @@ export default function Error({
 
   return (
     <>
-      <div className="w-full min-h-dvh p-6 bg-blue-100">
+      <div className="w-full min-h-dvh p-6 bg-blue-100 max-md:p-4">
         <SectionField sectionTitle="Error">
           <div>
             <p className="mb-2">{error.message}</p>
--- app/loading.tsx
+++ app/loading.tsx
@@ -3,7 +3,7 @@ import Loading from './components/layouts/Loading';
 
 const LoadingPage = () => {
   return(
-    <div className="flex justify-center items-center w-full min-h-dvh p-6 bg-blue-100">
+    <div className="flex justify-center items-center w-full min-h-dvh p-6 bg-blue-100 max-md:p-4">
       <Loading />
     </div>
   )
