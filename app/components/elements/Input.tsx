bed16ba0542c04fc6fa1bcd477f87d14b216b0d7
AgawaToshiki <t.agawa1128ht@gmail.com>
2024-12-16 23:08:00 +0900

    汎用的なInputコンポーネント、Selectコンポーネントの作成

---
+++ app/components/elements/Input.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  errorMessage?: string;
+}
+
+const Input = ({ errorMessage, ...props }: InputProps) => {
+
+  const baseStyle = "w-full border rounded-sm border-gray-200 shadow-md block px-2 h-12 max-md:h-10 max-md:shadow-sm"
+  const errorStyle = `${errorMessage ? "border-red-400" : "border-gray-200"}`
+  const propsStyle = `${props.className ? props.className : ""}`
+
+  const inputStyle = [
+    baseStyle,
+    errorStyle,
+    propsStyle
+  ].filter(Boolean).join(' ');
+  
+  return (
+    <>
+      <input
+        {...props}
+        className={inputStyle}
+      />
+    </>
+  )
+}
+
+export default Input
---
+++ app/components/elements/Select.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+interface SelectProps extends React.SelectHTMLAttributes<HTMLSelectElement> {
+  errorMessage?: string;
+  options: {
+    value: string;
+    label: string;
+  }[];
+}
+
+const Select = ({ errorMessage, options, ...props }: SelectProps) => {
+
+  const baseStyle = "w-full border rounded-sm shadow-md block px-2 h-12 bg-white max-md:h-10 max-md:shadow-sm"
+  const errorStyle = `${errorMessage ? "border-red-400" : "border-gray-200"}`
+  const propsStyle = `${props.className ? props.className : ""}`
+
+  const selectStyle = [
+    baseStyle,
+    errorStyle,
+    propsStyle
+  ].filter(Boolean).join(' ');
+  
+  return (
+    <>
+      <select
+        {...props}
+        className={selectStyle}
+      >
+        {props.value === "" && (
+          <option value="" selected disabled hidden>選択してください</option>
+        )}
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    </>
+  )
+}
+
+export default Select
