a8c2c1485e55d60151308598859f9184414a2370
AgawaToshiki <t.agawa1128ht@gmail.com>
2024-12-19 18:07:32 +0900

    使っていないAPIメソッドを削除

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -8,63 +8,6 @@ import { registerValidation } from "@/app/utils/validation";
 import { isAdminUser } from '@/app/utils/functions';
 
 
-export async function GET(req: NextRequest) {
-  try {
-
-    if(req.method !== "GET"){
-      throw new APIError(405, 'Method Not Allowed');
-    }
-    
-    const authUser = await getCurrentUser();
-    if(!authUser || !authUser.id){
-      throw new APIError(401, 'Unauthorized user');
-    }
-    const user = await getUser(authUser.id);
-    if(!user){
-      throw new APIError(401, 'Unauthorized user');
-    }
-    const isAdmin = isAdminUser(user);
-    if(!isAdmin) {
-      throw new APIError(403, 'Permission denied');
-    }
-
-    const data: { email: string, password: string, displayName: string } = await req.json();
-
-    if(!data) {
-      throw new APIError(400, 'BadRequest');
-    }
-
-    const { isValid } = registerValidation(data.email, data.password, data.displayName);
-
-    if(!isValid) {
-      throw new APIError(400, 'Invalid user data');
-    }
-  
-    const userData = await createUserFromAuth(data.email, data.password);
-
-    if(!userData || !userData.id) {
-      throw new APIError(422, 'User creation failed');
-    }
-
-    await registerUser(userData.id, data.email, data.displayName);
-    
-    return NextResponse.json({ status: 201 });
-
-  }catch (error) {
-    if(error instanceof APIError) {
-      return NextResponse.json(
-        { error: error.message },
-        { status: error.statusCode }
-      );
-    }
-    return NextResponse.json(
-      { error: 'Internal Server Error' },
-      { status: 500 }
-    );
-  }
-}
-
-
 export async function POST(req: NextRequest) {
   try {
 
